9a985059417731a0758a2a52b4a0533ad8ed31c3
Piotr Wojtaszczyk <piotr.wojtaszczyk@timesys.com>
2022-01-05 20:25:47 +0100

    fixups

--- src/platform/icap_bm_rpmsg-lite.c
+++ src/platform/icap_bm_rpmsg-lite.c
@@ -9,18 +9,15 @@
 
 typedef uint16_t atomic_t;
 
-struct _rpmsg_msg{
-	uint8_t *payload;
-	uint32_t payload_len;
-};
-struct _rpmsg_queue {
+
+struct _icap_msg_fifo {
 	atomic_t head;
 	atomic_t tail;
-	struct _rpmsg_msg msg[ICAP_MSG_QUEUE_SIZE];
+	struct icap_msg *msg[ICAP_MSG_QUEUE_SIZE];
 	uint32_t in_use;
 };
 
-static struct _rpmsg_queue rpmsg_msg_queues[ICAP_MAX_ICAP_INSTANCES] = {{0}};
+static struct _icap_msg_fifo icap_response_fifo[ICAP_MAX_ICAP_INSTANCES] = {{0}};
 
 
 int32_t icap_init_transport(struct icap_instance *icap)
@@ -75,10 +72,11 @@ int32_t icap_send_platform(struct icap_instance *icap, void *data, uint32_t size
 			data, size, 0);
 }
 
-int32_t icap_response_notify(struct icap_instance *icap, void *data, uint32_t size)
+int32_t icap_response_notify(struct icap_instance *icap, struct icap_msg *response)
 {
 	struct icap_rpmsg_lite_ep_info *ept_info = (struct icap_rpmsg_lite_ep_info *)icap->transport;
 	struct _rpmsg_queue *queue = (struct _rpmsg_queue*)ept_info->priv;
+	uint32_t size;
 
 	atomic_t head_next = queue->head + 1;
 	if (head_next >= ICAP_MSG_QUEUE_SIZE){
@@ -91,8 +89,9 @@ int32_t icap_response_notify(struct icap_instance *icap, void *data, uint32_t si
 	}
 
 	// put the message to the queue
-	queue->msg[head_next].payload = data;
-	queue->msg[head_next].payload_len = size;
+	size = sizeof(struct icap_msg_header) + response->header.payload_len
+	queue->msg[head_next].response = response;
+	queue->msg[head_next].response_len = size;
 	queue->head = head_next;
 	return RL_HOLD;
 }
@@ -123,20 +122,18 @@ int32_t icap_wait_for_response(struct icap_instance *icap, uint32_t seq_num, str
 			tail_next = 0;
 		}
 
-		msg = (struct icap_msg *)queue->msg[tail_next].payload;
-		size = queue->msg[tail_next].payload_len;
+		msg = queue->msg[tail_next].response;
+		size = queue->msg[tail_next].response_len;
 
-		if ( ((msg->header.payload_len + sizeof(struct icap_msg_header)) == size)
-				&& ((msg->header.type == ICAP_ACK) || (msg->header.type == ICAP_NAK))
-				&& (msg->header.seq_num == seq_num)	) {
+		if (msg->header.seq_num == seq_num) {
 			/* Got proper response */
 			memcpy(response, msg, size);
-			rpmsg_lite_release_rx_buffer(ept_info->rpmsg_instance, queue->msg[tail_next].payload);
+			rpmsg_lite_release_rx_buffer(ept_info->rpmsg_instance, queue->msg[tail_next].response);
 			queue->tail = tail_next;
 			return 0;
 		}
 
-		rpmsg_lite_release_rx_buffer(ept_info->rpmsg_instance, queue->msg[tail_next].payload);
+		rpmsg_lite_release_rx_buffer(ept_info->rpmsg_instance, queue->msg[tail_next].response);
 		queue->tail = tail_next;
 	}
 }
